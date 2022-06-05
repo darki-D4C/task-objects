5c73f4d0b7663d7a92ce5d62f9dcf1d824c1e845
darki-D4C <den.pashmentov75@yandex.ru>
2022-06-05 12:55:52 +0300

    task is done

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,14 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+  if (data.gender == 'female') {
+    delete data.age;
+  } else if (data.gender == 'male') {
+    if (!data.hasOwnProperty('income')) {
+      data.income = 100000;
+    }
+  }
+  return data;
 }
 
 /*
@@ -15,6 +23,12 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+  let keys3 = Object.keys(obj3);
+  let newArray = [].concat(keys1, keys2, keys3);
+  newArray.sort();
+  return newArray;
 }
 
 /*
@@ -23,4 +37,12 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+  let arr = [],
+    i = 0;
+  for (; i < count; i++) {
+    let cloneObj = JSON.parse(JSON.stringify(obj));
+    cloneObj.id = i;
+    arr.push(cloneObj);
+  }
+  return arr;
 }
